a28e389d1ea2ed160a074c027d42bbce09f4878d
Dhruv Malani <118156335+Malanidhruv@users.noreply.github.com>
2025-03-28 12:27:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from stock_analysis import (
 from stock_lists import STOCK_LISTS
 
 st.set_page_config(page_title="Stock Screener", layout="wide")
-st.warning("This screener is based on statistical analysis. Please conduct your own due diligence before making any trading decisions. This application is best compatible with **Google Chrome**.")
+st.warning("Im Dhruv Malani This screener is based on statistical analysis. Please conduct your own due diligence before making any trading decisions. This application is best compatible with **Google Chrome**.")
 
 # Try loading stored credentials
 user_id, api_key = load_credentials()
